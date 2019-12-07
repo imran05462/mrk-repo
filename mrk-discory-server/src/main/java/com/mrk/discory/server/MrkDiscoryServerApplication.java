ec371894d38b29dd8b1d0a3b3068ae5c05edf01d
a <imran05462@gmail.com>
2019-12-07 12:04:15 +0530

    first commit

---
+++ mrk-discory-server/src/main/java/com/mrk/discory/server/MrkDiscoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.mrk.discory.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrkDiscoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrkDiscoryServerApplication.class, args);
+	}
+
+}
